e3b7cdc902a13c79945d032e53fc5326ddaa2d63
Aleksey Kononenko <aleksey19988@gmail.com>
2021-06-18 12:59:44 +0300

    адаптировал изменение цвета крестика для закрытия меню в зависимости от активной темы страницы (тёмная/светлая)

--- scripts/themeToggle.js
+++ scripts/themeToggle.js
@@ -2,6 +2,7 @@ const themeToggleBtn = document.querySelector('.theme-toggle-and-nav__toggle');
 const links = document.querySelectorAll('a');
 const navList = document.querySelector('.nav-list');
 const navLinks = navList.querySelector('a');
+const closeNavBtn = document.querySelector('.close-nav-list-btn');
 
 themeToggleBtn.addEventListener('click', () => {
     const bodyBackGroundColor = window.getComputedStyle(document.body).backgroundColor;//Вычисляем текущий цвет темы
@@ -14,6 +15,7 @@ themeToggleBtn.addEventListener('click', () => {
         }
         navList.style.backgroundColor = '#fff';
         navLinks.style.color = '#000';
+        closeNavBtn.style.color = '#000';
     } else {
         document.body.style.backgroundColor = '#000';
         document.body.style.color = '#fff';
@@ -22,5 +24,6 @@ themeToggleBtn.addEventListener('click', () => {
         }
         navList.style.backgroundColor = '#000';
         navLinks.style.color = '#fff';
+        closeNavBtn.style.color = '#fff';
     }
 });
